20724a91bec45077345d80549220b6c91a0adee7
Jhessica Barrios Muni <201806081@est.umss.edu>
2025-09-25 16:08:38 -0300

    logica para obtener un estudiante

--- SistemaPostulantes/src/main/java/com/example/sistemapostulantes/model/ConexionBaseDatos.java
+++ SistemaPostulantes/src/main/java/com/example/sistemapostulantes/model/ConexionBaseDatos.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class ConexionBaseDatos {
     private static Connection connection;
-    private static final String URL = "jdbc:mysql://avnadmin:AVNS_l0YRyO-laQz0uaAmDF2@mysql-sisinfo2.d.aivencloud.com:11329/defaultdb?ssl-mode=REQUIRED";
+    private static final String URL = "jdbc:mysql://avnadmin:AVNS_l0YRyO-laQz0uaAmDF2@mysql-sisinfo2.d.aivencloud.com:11329/Postulantes?ssl-mode=REQUIRED";
     private static final String USUARIO = "avnadmin";
     private static final String CONTRASENIA = "AVNS_l0YRyO-laQz0uaAmDF2";
 

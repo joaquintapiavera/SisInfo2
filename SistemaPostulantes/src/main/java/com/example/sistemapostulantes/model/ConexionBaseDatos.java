d76da1bfb898141d14a7636745c0e302c52a6712
kiwiiii-cr <iwancq09@gmail.com>
2025-09-28 22:40:40 -0400

    Update ConexionBaseDatos.java

--- SistemaPostulantes/src/main/java/com/example/sistemapostulantes/model/ConexionBaseDatos.java
+++ SistemaPostulantes/src/main/java/com/example/sistemapostulantes/model/ConexionBaseDatos.java
@@ -5,11 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBaseDatos {
-    private static final String URL = "jdbc:mysql://mysql-sisinfo2.d.aivencloud.com:11329/Postulantes?ssl-mode=REQUIRED";
+
+
+    private static final String URL =
+            "jdbc:mysql://mysql-sisinfo2.d.aivencloud.com:11329/Postulantes?sslMode=REQUIRED&serverTimezone=UTC";
     private static final String USUARIO = "avnadmin";
     private static final String CONTRASENIA = "AVNS_l0YRyO-laQz0uaAmDF2";
 
     public static Connection getConexion() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver MySQL no encontrado", e);
+        }
         return DriverManager.getConnection(URL, USUARIO, CONTRASENIA);
     }
 }
